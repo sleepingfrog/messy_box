a789a1ee3681e99cc22716937c4e3d97d7f6ea06
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2020-10-28 14:57:19 +0000

    testing

---
+++ test/components/user_name_component_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class UserNameComponentTest < ViewComponent::TestCase
+  def test_render_component
+    user = User.new(name: 'user1')
+    result = render_inline(UserNameComponent.new(user: user))
+
+    assert_includes result.css('div span').to_html, user.name
+  end
+end
---
+++ test/concepts/user/cell/name_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class UserCellNameTest < ActiveSupport::TestCase
+  def test_render_cell
+    user = User.new(name: 'user1')
+    result = User::Cell::Name.call(user).call
+    doc = Nokogiri::HTML(result)
+
+    assert_includes doc.css('div span').to_html, user.name
+  end
+end
