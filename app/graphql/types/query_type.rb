acdbaee20f6a541fd46befd4641fc066d75b7c0e
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2020-11-15 04:33:08 +0900

    user_todo query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,9 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :user_todos, [Types::UserTodoType], null: true
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def user_todos
+      context[:current_user]&.todos
     end
   end
 end
