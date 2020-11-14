1e5a2bbdf88998a2025a53879f0b85a213d042ab
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2020-11-15 04:01:33 +0900

    update model

--- app/models/user_todo.rb
+++ app/models/user_todo.rb
@@ -1,3 +1,8 @@
 class UserTodo < ApplicationRecord
   belongs_to :user
+
+  with_options presence: true do
+    validates :content
+    validates :position
+  end
 end
--- db/migrate/20201114184633_create_user_todos.rb
+++ db/migrate/20201114184633_create_user_todos.rb
@@ -2,10 +2,12 @@ class CreateUserTodos < ActiveRecord::Migration[6.0]
   def change
     create_table :user_todos do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :content
-      t.integer :position
+      t.string :content, null: false
+      t.integer :position, null: false
 
       t.timestamps
+
+      t.index [:user_id, :position], unique: true
     end
   end
 end
