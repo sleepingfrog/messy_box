e9b4da7980738c2c1d712a61551096229d217544
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2020-11-23 14:54:25 +0900

    add user_todo seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
-User.create(email: 'user1@example.org', name: 'user1', password: 'password')
+user1 = User.create(email: 'user1@example.org', name: 'user1', password: 'password')
 User.create(email: 'user2@example.org', name: 'user2', password: 'password')
 User.create(email: 'user3@example.org', name: 'user3', password: 'password')
+
+1.upto(5) do |i|
+  user1.todos.build(position: i, content: 'todo')
+end
+user1.save!
