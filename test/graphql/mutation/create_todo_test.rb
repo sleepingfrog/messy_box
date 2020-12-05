dbf0ce019406b4b5738d2597e34b928f470079d7
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2020-12-05 13:07:13 +0900

    mutation test

---
+++ test/graphql/mutation/create_todo_test.rb
@@ -0,0 +1,43 @@
+require 'test_helper'
+
+class GraphqlMutationCreateTodoTest < ActiveSupport::TestCase
+  setup do
+    @user = create(:user)
+    @query = <<~QUERY
+      mutation createTodo($input: CreateTodoInput!){
+        createTodo(input: $input){
+          todo {
+            id
+            content
+            position
+          }
+          errors
+        }
+     }
+    QUERY
+  end
+
+  test 'valid variables' do
+    variables = { input: { content: 'todo string' } }
+    result = nil
+
+    assert_difference('UserTodo.count', 1) do
+      assert_difference('@user.todos.count', 1) do
+        result = MessyBoxSchema.execute(@query, context: { current_user: User.find(@user.id) }, variables: variables)
+      end
+    end
+
+    assert_equal variables.dig(:input, :content), result.dig('data', 'createTodo', 'todo', 'content')
+  end
+
+  test 'content blank' do
+    variables = { input: { content: nil } }
+    result = nil
+
+    assert_no_difference('UserTodo.count') do
+      assert_no_difference('@user.todos.count') do
+        result = MessyBoxSchema.execute(@query, context: { current_user: User.find(@user.id) }, variables: variables)
+      end
+    end
+  end
+end
