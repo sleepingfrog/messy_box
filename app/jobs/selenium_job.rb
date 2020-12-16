7b5a6ad085fdc9b379ffd7a982ffc4076ebe18f1
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2020-12-16 13:32:31 +0900

    selenium job

--- app/jobs/selenium_job.rb
+++ app/jobs/selenium_job.rb
@@ -1,7 +1,25 @@
+require 'selenium-webdriver'
+
 class SeleniumJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # Do something later
+    uri = args.first
+    driver = setup_driver
+    driver.get(uri)
+    screenshot = driver.screenshot_as(:png)
+
+    # stub
+    File.open("./tmp/#{job_id}.png", 'wb') { |f| f.write(screenshot) }
   end
+
+  private
+
+    def setup_driver
+      driver = Selenium::WebDriver.for(:remote, url: selenium_url, desired_capabilities: :firefox)
+    end
+
+    def selenium_url
+      ENV['SELENIUM_URL']
+    end
 end
