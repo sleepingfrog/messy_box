5f0d87c4cb70e1de75729d3f033ec55e9ce062c8
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2021-01-01 03:06:19 +0900

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_14_184633) do
+ActiveRecord::Schema.define(version: 2020_12_31_180111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_tags_on_name", unique: true
+  end
 
   create_table "user_todos", force: :cascade do |t|
     t.bigint "user_id", null: false
--- test/factories/user_todos.rb
+++ test/factories/user_todos.rb
@@ -1,4 +1,25 @@
 # frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: user_todos
+#
+#  id         :bigint           not null, primary key
+#  content    :string           not null
+#  position   :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_user_todos_on_user_id               (user_id)
+#  index_user_todos_on_user_id_and_position  (user_id,position) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 FactoryBot.define do
   sequence :position
 
