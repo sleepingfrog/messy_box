a3dc8aae08a5639ad6ab73ed2c4a0c70962f0235
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2021-01-02 01:25:17 +0900

    elasticsearch-model

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,5 +9,37 @@
 #  updated_at :datetime         not null
 #
 class Article < ApplicationRecord
+  include Elasticsearch::Model
+
   has_and_belongs_to_many :tags
+
+  index_name "es_article_#{Rails.env}"
+
+  settings do
+    mappings dynamic: false do
+      indexes :id, type: 'integer'
+      indexes :title, type: 'text', analyzer: 'kuromoji'
+      indexes :body, type: 'text', analyzer: 'kuromoji'
+      indexes :tags do
+        indexes :name, type: 'keyword'
+      end
+    end
+  end
+
+  def as_indexed_json(*)
+    as_json(include: { tags: { only: :name } })
+  end
+
+  def self.create_index!
+    client = __elasticsearch__.client
+    client.indices.delete(index: self.index_name) rescue nil
+
+    client.indices.create(
+      index: self.index_name,
+      body: {
+        settings: self.settings.to_hash,
+        mappings: self.mappings.to_hash,
+      }
+    )
+  end
 end
