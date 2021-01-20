b2a14caff46fa89ce3939addb3d6fcac779dafd3
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2021-01-20 17:52:56 +0900

    search tag

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,7 +3,7 @@ module Types
   class QueryType < Types::BaseObject
     field :user_todos, [Types::UserTodoType], null: true
     field :articles, Types::ArticleConnectionWithTotalCountType, null: true, connection: true do
-      argument :query, String, required: false
+      argument :query, Types::ArticleQueryType, required: false
     end
 
     def user_todos
@@ -11,16 +11,30 @@ module Types
     end
 
     def articles(query: nil)
-      query = if query.present?
+      value_query = if query.value.present?
                 {
                   multi_match: {
                     fields: ['title', 'body'],
                     type: 'cross_fields',
                     operator: 'and',
-                    query: query,
+                    query: query.value,
                   }
                 }
               end
+
+      tags_query = if query.tags.present?
+                     {
+                       terms: {
+                         'tags.name': query.tags
+                       }
+                     }
+                   end
+
+      query = {
+        bool: {
+          must: [value_query, tags_query].compact
+        }
+      }
 
       ElasticsearchRepository.new(Article, query)
     end
