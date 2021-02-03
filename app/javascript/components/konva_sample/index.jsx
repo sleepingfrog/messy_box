27007a222504f81c84b9deed772c249b20dcfad2
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2021-02-04 00:05:13 +0900

    snap drag

--- app/javascript/components/konva_sample/index.jsx
+++ app/javascript/components/konva_sample/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { Rect, Layer, Stage, Line } from 'react-konva';
 
@@ -34,38 +34,97 @@ const GridLayer = () => {
   )
 }
 
-const Frame = ({id, x, y, width, height, color}) => {
+const Frame = ({id, x, y, width, height, color, handleDragEnd, handleDragStart, handleDragMove}) => {
   const page = useContext(PageContext);
   const blockSize = {
     width: page.width / page.xCount,
     height: page.height / page.yCount,
   }
+  const [isDragging, setIsDragging] = useState(false)
+  const [position, setPosition] = useState({
+    x: x * blockSize.width,
+    y: y * blockSize.height,
+  })
+
+  useEffect(() => {
+    setPosition({
+      x: x * blockSize.width,
+      y: y * blockSize.height,
+    })
+  }, [x, y, isDragging])
+
+  const calcPosition = ({x, y}) => ({
+      x: Math.round(x / blockSize.width),
+      y: Math.round(y / blockSize.height),
+  })
+
+  const onDragEnd = (e) => {
+    const position = calcPosition({x: e.target._lastPos.x, y: e.target._lastPos.y})
+    handleDragEnd(e, id, position);
+    setIsDragging(false)
+  }
+
+  const onDragMove = (e) => {
+    const position = calcPosition({x: e.target._lastPos.x, y: e.target._lastPos.y})
+    handleDragMove(e, id, position);
+    setPosition({
+      x: e.target._lastPos.x,
+      y: e.target._lastPos.y,
+    })
+  }
+  const onDragStart = (e) => {
+    setIsDragging(true)
+    handleDragStart(e, id)
+  }
+
   return(
     <Rect
       key={id}
-      x={x * blockSize.width}
-      y={y * blockSize.height}
+      x={position.x}
+      y={position.y}
       width={width * blockSize.width}
       height={height * blockSize.height}
-      fill={color}
+      fill={isDragging ? '#ccccFF' : color}
+      draggable={true}
+      onDragEnd={onDragEnd}
+      onDragStart={onDragStart}
+      onDragMove={onDragMove}
+      _useStrictMode
     />
   )
 }
 
-const FrameLayer = ({frames}) => {
-  const renderFrames = () => (
-    frames.map((frame) => (
-      <Frame key={frame.id} {...frame}/>
-    ))
-  )
-
+const FrameLayer = ({frames, handleDragStart, handleDragEnd, handleDragMove}) => {
   return(
     <Layer>
       {
         frames.map((frame) =>(
-          <Frame key={frame.id} {...frame} />
+          <Frame key={frame.id} {...frame} handleDragStart={handleDragStart} handleDragEnd={handleDragEnd} handleDragMove={handleDragMove} />
         ))
       }
+    </Layer>
+  )
+}
+
+const ShadowLayer = ({shadow}) => {
+  if(!shadow) { return null }
+
+  const page = useContext(PageContext);
+  const blockSize = {
+    width: page.width / page.xCount,
+    height: page.height / page.yCount,
+  }
+
+  return(
+    <Layer>
+      <Rect
+        x={shadow.x * blockSize.width}
+        y={shadow.y * blockSize.height}
+        width={shadow.width * blockSize.width}
+        height={shadow.height * blockSize.height}
+        fill={'#8888'}
+        _useStrictMode
+      />
     </Layer>
   )
 }
@@ -84,11 +143,38 @@ export default () => {
     yCount: 8,
   }
 
+  const [frames, setFrames] = useState(INITIAL_FRAMES);
+  const [shadow, setShadow] = useState(null);
+
+  const handleDragEnd = (e, frameId, position) => {
+    const frame = frames.find(({id}) => frameId === id)
+    setFrames(
+      [
+        ...frames.filter(({id}) => id !== frameId), {...frame, x: position.x, y: position.y}
+      ]
+    )
+  }
+
+  const handleDragMove = (e, frameId, position) => {
+    const frame = frames.find(({id}) => frameId === id)
+    setShadow({...frame, x: position.x, y: position.y})
+  }
+
+  const handleDragStart = (e, frameId) => {
+    const frame = frames.find(({id}) => frameId === id)
+    setFrames(
+      [
+        ...frames.filter(({id}) => id !== frameId), frame
+      ]
+    )
+  }
+
   return(
     <Stage width={pageSetting.width} height={pageSetting.height}>
       <PageContext.Provider value={pageSetting}>
         <GridLayer />
-        <FrameLayer frames={INITIAL_FRAMES} />
+        <ShadowLayer shadow={shadow} />
+        <FrameLayer frames={frames} handleDragStart={handleDragStart} handleDragEnd={handleDragEnd} handleDragMove={handleDragMove}/>
       </PageContext.Provider>
     </Stage>
   )
