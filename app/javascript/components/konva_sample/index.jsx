edf979b366c501115fa56c19ac64f66284d638c8
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2021-02-03 21:53:19 +0900

    frame component

--- app/javascript/components/konva_sample/index.jsx
+++ app/javascript/components/konva_sample/index.jsx
@@ -34,24 +34,47 @@ const GridLayer = () => {
   )
 }
 
-const FrameLayer = ({frames}) => {
+const Frame = ({id, x, y, width, height, color}) => {
   const page = useContext(PageContext);
   const blockSize = {
     width: page.width / page.xCount,
     height: page.height / page.yCount,
   }
+  return(
+    <Rect
+      key={id}
+      x={x * blockSize.width}
+      y={y * blockSize.height}
+      width={width * blockSize.width}
+      height={height * blockSize.height}
+      fill={color}
+    />
+  )
+}
+
+const FrameLayer = ({frames}) => {
   const renderFrames = () => (
-    frames.map(({x, y, width, height, color}, index) => (
-      <Rect key={index} x={x * blockSize.width} y={y * blockSize.height} width={width * blockSize.width} height={height * blockSize.height} fill={color} />
+    frames.map((frame) => (
+      <Frame key={frame.id} {...frame}/>
     ))
   )
 
   return(
     <Layer>
-      {renderFrames()}
+      {
+        frames.map((frame) =>(
+          <Frame key={frame.id} {...frame} />
+        ))
+      }
     </Layer>
   )
 }
+
+const INITIAL_FRAMES = [
+  { id: 0, x: 0, y: 0, width: 1, height: 2, color: '#ffbbee' },
+  { id: 1, x: 1, y: 3, width: 8, height: 2, color: '#123123' },
+  { id: 2, x: 3, y: 1, width: 1, height: 3, color: '#FF0012' },
+]
 
 export default () => {
   const pageSetting = {
@@ -61,17 +84,11 @@ export default () => {
     yCount: 8,
   }
 
-  const frames = [
-    { x: 0, y: 0, width: 1, height: 2, color: '#ffbbee' },
-    { x: 1, y: 3, width: 8, height: 2, color: '#123123' },
-    { x: 3, y: 1, width: 1, height: 3, color: '#FF0012' },
-  ]
-
   return(
     <Stage width={pageSetting.width} height={pageSetting.height}>
       <PageContext.Provider value={pageSetting}>
         <GridLayer />
-        <FrameLayer frames={frames} />
+        <FrameLayer frames={INITIAL_FRAMES} />
       </PageContext.Provider>
     </Stage>
   )
