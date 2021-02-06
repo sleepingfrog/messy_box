695ebb4a693b959a24299ba3c91426be2b6e03f1
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2021-02-07 03:45:00 +0900

    annotate model tests

--- test/models/chapter_test.rb
+++ test/models/chapter_test.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: chapters
+#
+#  id          :bigint           not null, primary key
+#  page_count  :integer
+#  page_offset :integer
+#  position    :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  book_id     :bigint           not null
+#
+# Indexes
+#
+#  index_chapters_on_book_id               (book_id)
+#  index_chapters_on_book_id_and_position  (book_id,position) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (book_id => books.id)
+#
 require 'test_helper'
 
 class ChapterTest < ActiveSupport::TestCase
--- test/models/page_test.rb
+++ test/models/page_test.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: pages
+#
+#  id         :bigint           not null, primary key
+#  number     :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  chapter_id :bigint           not null
+#
+# Indexes
+#
+#  index_pages_on_chapter_id             (chapter_id)
+#  index_pages_on_chapter_id_and_number  (chapter_id,number) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (chapter_id => chapters.id)
+#
 require 'test_helper'
 
 class PageTest < ActiveSupport::TestCase
