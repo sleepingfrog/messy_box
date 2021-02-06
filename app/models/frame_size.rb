d724a1386cc0d7fbe030052843af78a5e8369052
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2021-02-07 04:16:58 +0900

    rails db:migrate

--- app/models/frame_size.rb
+++ app/models/frame_size.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: frame_sizes
+#
+#  id         :bigint           not null, primary key
+#  height     :integer          not null
+#  name       :text             not null
+#  width      :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class FrameSize < ApplicationRecord
   validates :width, :height, :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_185606) do
+ActiveRecord::Schema.define(version: 2021_02_06_191357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,14 @@ ActiveRecord::Schema.define(version: 2021_02_04_185606) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["book_id", "position"], name: "index_chapters_on_book_id_and_position", unique: true
     t.index ["book_id"], name: "index_chapters_on_book_id"
+  end
+
+  create_table "frame_sizes", force: :cascade do |t|
+    t.text "name", null: false
+    t.integer "width", null: false
+    t.integer "height", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "pages", force: :cascade do |t|
