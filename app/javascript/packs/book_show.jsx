fb756be751d534cfc185200f24a3eea1aaac21dc
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2021-02-24 07:35:25 +0900

    book info

--- app/javascript/packs/book_show.jsx
+++ app/javascript/packs/book_show.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ReactDOM from 'react-dom';
+import { createCache, createClient } from '../utils/apollo';
+import { ApolloProvider, useQuery, gql  } from '@apollo/client';
 import {
   BrowserRouter as Router,
   Link,
@@ -9,56 +11,83 @@ import {
   useParams
 } from 'react-router-dom';
 
+const BOOK_QUERY = gql`
+  query BookType($id: ID!) {
+    book(id: $id) {
+      id
+      title
+      description
+      chapters {
+        position
+        pageCount
+      }
+    }
+  }
+`
+
 const App = () => {
   return(
     <>
       <div>books show</div>
-      <Router>
-        <Book />
-      </Router>
+      <ApolloProvider client={createClient(createCache())}>
+        <Router>
+          <BookRoute />
+        </Router>
+      </ApolloProvider>
     </>
   )
 }
 
-function Book(){
+function BookRoute() {
   let match = useRouteMatch('/books/:bookId');
   return(
-    <>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to={`${match.url}/pages/1`}>Page1</Link>
-            </li>
-            <li>
-              <Link to={`${match.url}/pages/2`}>Page2</Link>
-            </li>
-            <li>
-              <Link to={`${match.url}/pages/3`}>Page3</Link>
-            </li>
-            <li>
-              <Link to={`${match.url}`}>Home</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-
-      <Switch>
-        <Route path={`${match.path}/pages/:pageNumber`}>
-          <Pages />
-        </Route>
-        <Route path={match.path}>
-          <h3>home</h3>
-        </Route>
-      </Switch>
-    </>
+    <Switch>
+      <Route path={match.path} >
+        <Book />
+      </Route>
+    </Switch>
   )
 }
 
-function Pages() {
-  let { bookId, pageNumber }= useParams()
+const BookContext = React.createContext({
+  title: "",
+  description: "",
+  chapters: [],
+})
+
+function Book(){
+  const { bookId } = useParams();
+  const { loading, error, data } = useQuery(BOOK_QUERY, {
+    variables: {id: bookId}
+  })
+
+  if (loading) { return("loading...") }
+  if (error) { return("error...") }
+
   return(
-    <div>book: {bookId} pages: {pageNumber}</div>
+    <BookContext.Provider value={data.book}>
+      <BookInfo />
+    </BookContext.Provider>
+  )
+}
+
+function BookInfo() {
+  const data = useContext(BookContext);
+  const renderChapterInfo = () => {
+    return(
+      <div> chapters
+        {data.chapters.map(({position, pageCount}) => <div key={position}> <div>position: {position} count: {pageCount}</div></div>)
+        }
+      </div>
+    )
+  }
+
+  return(
+    <div>
+      <div> title: {data.title} </div>
+      <div> description: {data.description} </div>
+      {renderChapterInfo()}
+    </div>
   )
 }
 
