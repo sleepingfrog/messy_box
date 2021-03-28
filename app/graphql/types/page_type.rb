d1a6ccc2d2963ea7360a33742b7f009f3c59df27
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2021-03-28 22:47:37 +0900

    page_size query

--- app/graphql/types/page_type.rb
+++ app/graphql/types/page_type.rb
@@ -2,6 +2,11 @@ module Types
   class PageType < Types::BaseObject
     field :number, Integer, null: false
     field :frames, [Types::FrameType], null: false
+    field :page_size, Types::PageSizeType, null: false
+
+    def page_size
+      Loaders::AssociationLoader.for(Page, :page_size).load(object)
+    end
 
     def frames
       Loaders::AssociationLoader.for(Page, :frames).load(object)
