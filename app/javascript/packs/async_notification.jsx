9c921e0654c901756b05c9009f4f593f97e5470c
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2021-04-27 12:40:07 +0900

    tiny sample

--- app/javascript/packs/async_notification.jsx
+++ app/javascript/packs/async_notification.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect  } from 'react';
 import ReactDOM from 'react-dom';
+import consumer from "../channels/consumer"
 
 const App = () => {
+  useEffect(() => {
+    consumer.subscriptions.create({
+      channel: 'AsyncNotificationChannel'
+    },
+    {
+      received(data) { console.log(data) }
+    })
+  })
+
   return(
     <div>
       hello
