b4189e30513e3707c08441ee1c9dc9b376cdf247
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2021-04-29 17:58:36 +0900

    sample
    ```ruby
    ActionCable.server.broadcast('async_notification', 'something')
    ```

--- app/javascript/packs/async_notification.jsx
+++ app/javascript/packs/async_notification.jsx
@@ -11,31 +11,52 @@ const App = () => {
 
   return(
     <div>
+      receive { receive.toString() }<button type='button' onClick={onClick} >or not</button>
       { receive && <Notification /> }
-      receive <button type='button' onClick={onClick} >{ receive.toString() }</button>
     </div>
   )
 }
 
+const NOTIFICATION_SIZE = 10
+
 const Notification = () => {
+  const [notifications, setNotifications] = useState([]);
+  const appendNotification = (data) => {
+    setNotifications((prev) => {
+      console.log('received', data)
+      if (prev.length >= NOTIFICATION_SIZE) {
+        return([...prev.slice(prev.length - (NOTIFICATION_SIZE - 1)), data])
+      } else {
+        return([...prev, data])
+      }
+    })
+  }
+  const renderNotifications = () => {
+    return(
+      notifications.map(data => <div key={data}>{data}</div> )
+    )
+  }
+
   useEffect(() => {
     const subscription = consumer.subscriptions.create({
       channel: 'AsyncNotificationChannel'
     },
     {
-      received(data) { console.log(data) }
+      received(data) { appendNotification(data) }
     })
 
     const cleanup = () => {
+      console.log('cleanup!')
       consumer.subscriptions.remove(subscription)
     }
 
     return cleanup;
-  })
+  }, [])
 
   return(
     <div>
       Cable
+      { renderNotifications() }
     </div>
   )
 }
