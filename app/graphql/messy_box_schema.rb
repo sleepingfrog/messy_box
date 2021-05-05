e717826da83bda9c1e1f9ab8a55bb42e85a358b0
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2021-05-05 18:25:35 +0900

    Revert "Merge pull request #6 from sleepingfrog/update_ruby_to_3.0.1"
    This reverts commit e842f4e2ff596138315c57b343b302e89e5f798f, reversing
    changes made to 5743fe74627db311ca7ac11793222117ec1ed133.

--- app/graphql/messy_box_schema.rb
+++ app/graphql/messy_box_schema.rb
@@ -3,6 +3,13 @@ class MessyBoxSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
 
+  # Opt in to the new runtime (default in future graphql-ruby versions)
+  use GraphQL::Execution::Interpreter
+  use GraphQL::Analysis::AST
+
+  # Add built-in connections for pagination
+  use GraphQL::Pagination::Connections
+
   use GraphQL::Batch
 
   connections.add(ElasticsearchRepository, Connections::ElasticsearchConnection)
