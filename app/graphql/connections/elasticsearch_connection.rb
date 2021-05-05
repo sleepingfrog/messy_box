6a2daeae2854399d6d0bccf96fa3f912cd8d8e54
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2021-05-05 20:42:25 +0900

    fix ElasticsearchConnection
    エラーを修正
    ついでに 非推奨になってたRelayからPaginationに引っ越し

--- app/graphql/connections/elasticsearch_connection.rb
+++ app/graphql/connections/elasticsearch_connection.rb
@@ -1,32 +1,26 @@
 # frozen_string_literal: true
 module Connections
-  class ElasticsearchConnection < GraphQL::Relay::BaseConnection
-    def cursor_from_node(item)
-      item_index = paged_nodes_array.index(item)
-      if item_index.nil?
-        raise("Can't generate cursor, item not found in connection: #{item}")
-      else
-        offset = paged_nodes.from + item_index + 1
-        encode(offset.to_s)
-      end
+  class ElasticsearchConnection < GraphQL::Pagination::Connection
+    def nodes
+      paged_nodes_array
     end
 
     def has_next_page
-      es_total = total_count
+      total = total_count
 
-      if es_total && paged_nodes.from.present? && paged_nodes.size.present?
-        (paged_nodes.from + paged_nodes.size) < es_total
+      if total_count && paged_nodes.from.present? && paged_nodes.size.present?
+        (paged_nodes.from + paged_nodes.size) < total
       else
         false
       end
     end
 
     def has_previous_page
-      es_total = total_count
+      total = total_count
 
-      if es_total && paged_nodes.from.present? && paged_nodes.size.present?
+      if total && paged_nodes.from.present? && paged_nodes.size.present?
         from_cursor = paged_nodes.from - paged_nodes.size
-        0 <= from_cursor && from_cursor <= es_total
+        0 <= from_cursor && from_cursor <= total
       else
         false
       end
@@ -40,66 +34,73 @@ module Connections
       @facets ||= sliced_nodes.facets
     end
 
+    def cursor_for(item)
+      offset = nodes.index(item) + 1 + (@paged_nodes.from || 0)
+      encode(offset.to_s)
+    end
+
     private
 
-      def paged_nodes
-        return @paged_nodes if defined? @paged_nodes
+      def sliced_nodes
+        @sliced_nodes ||= begin
+          sliced_nodes = items
 
-        items = sliced_nodes
-
-        if first
-          items.size = first
-        end
-
-        if last
-          if items.size
-            if last <= items.size
-              from = items.from + items.size - last
-              items.from = from
-              items.size = last
-            end
-          else
-            from = items.from + total_count - [last, total_count].min
-            items.from = from
-            items.size = last
+          if after_offset
+            sliced_nodes.from = after_offset
           end
-        end
-        @paged_nodes = items
 
-        @paged_nodes
+          if before_offset && after_offset
+            sliced_nodes.size = if after_offset < before_offset
+              before_offset - after_offset - 1
+            else
+              0
+            end
+          elsif before_offset
+            sliced_nodes.size = before_offset - 1
+          end
+          sliced_nodes
+        end
       end
 
-      def sliced_nodes
-        return @sliced_nodes if defined? @sliced_nodes
+      def paged_nodes
+        @paged_nodes ||= begin
+          paged_nodes = sliced_nodes
 
-        @sliced_nodes = nodes
-
-        if after
-          after_i = offset_from_cursor(after)
-          @sliced_nodes.from = after_i
-        end
-
-        if before && after
-          @sliced_nodes.size = if offset_from_cursor(after) < offset_from_cursor(before)
-            offset_from_cursor(before) - offset_from_cursor(after) - 1
-          else
-            0
+          if first
+            paged_nodes.size = first
           end
 
-        elsif before
-          @sliced_nodes.size = offset_from_cursor(before) - 1
+          if last
+            if paged_nodes.size
+              if last <= paged_nodes.size
+                from = paged_nodes.from + paged_nodes.size - last
+                paged_nodes.from = from
+                paged_nodes.size = last
+              end
+            else
+              from = [(paged_nodes.from || 0), total_count - last].max
+              paged_nodes.from = from
+              paged_nodes.size = last
+            end
+          end
+          paged_nodes
         end
+      end
 
-        @sliced_nodes
+      def paged_nodes_array
+        @paged_nodes_array ||= paged_nodes.array
       end
 
       def offset_from_cursor(cursor)
         decode(cursor).to_i
       end
 
-      def paged_nodes_array
-        return @paged_nodes_array if defined?(@paged_nodes_array)
-        @paged_nodes_array = paged_nodes.array
+      def before_offset
+        @before_offset ||= before && offset_from_cursor(before)
+      end
+
+      def after_offset
+        @after_offset ||= after && offset_from_cursor(after)
       end
   end
 end
